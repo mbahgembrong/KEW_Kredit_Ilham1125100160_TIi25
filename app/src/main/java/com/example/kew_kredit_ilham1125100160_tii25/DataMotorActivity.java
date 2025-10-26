6986a209355cf1f34cf8646239a279146f14b2bd
Terabash <git@terabash.com>
2025-10-26 21:42:18 +0700

    WORKSHEET 4 - DESAIN DATA MOTOR ACTIVITY.pdf

--- app/src/main/java/com/example/kew_kredit_ilham1125100160_tii25/DataMotorActivity.java
+++ app/src/main/java/com/example/kew_kredit_ilham1125100160_tii25/DataMotorActivity.java
@@ -2,23 +2,328 @@ package com.example.kew_kredit_ilham1125100160_tii25;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class DataMotorActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DataMotorActivity extends AppCompatActivity implements OnClickListener {
+
+    //Inisialisasi Objek + Variabel + Class
+    Motor motor = new Motor();
+    TableLayout tbMotor;
+    Button btTambahMotor, btRefreshDataMotor;
+    ArrayList<Button> buttonEdit = new ArrayList<Button>();
+    ArrayList<Button> buttonDelete = new ArrayList<Button>();
+    JSONArray arrayMotor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_data_motor);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Mengizinkan operasi jaringan di main thread (hanya untuk debugging/emulator lama)
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        //Pemberian Nama komponen
+        tbMotor = (TableLayout) findViewById(R.id.tbMotor);
+        btTambahMotor = (Button) findViewById(R.id.btTambahMotor);
+        btRefreshDataMotor = (Button) findViewById(R.id.btRefreshDataMotor);
+
+        tampildataMotor();
+    }
+
+    //Metode KlikbtTambahMotor
+    public void KlikbtTambahMotor(View v) {
+        tambahMotor();
+    }
+
+    //Refresh Data: Me-restart activity untuk memuat ulang data
+    public void KlikbtRefreshDataMotor(View v) {
+        // restart acrtivity
+        finish();
+        startActivity(getIntent());
+    }
+
+    //Tampil data data motor
+    public void tampildataMotor() {
+        TableRow barisTabel = new TableRow(this);
+        barisTabel.setBackgroundColor(Color.BLACK);
+
+        // Memberi ID Header Tabel
+        TextView viewHeaderKdMotor = new TextView(this);
+        TextView viewHeaderNama = new TextView(this);
+        TextView viewHeaderHarga = new TextView(this);
+        TextView viewHeaderAction = new TextView(this);
+
+        // Memberi Nama kolom HEADER
+        viewHeaderKdMotor.setText("KdMotor");
+        viewHeaderNama.setText("Nama");
+        viewHeaderHarga.setText("Harga");
+        viewHeaderAction.setText("Action");
+
+        // Mengatur style header
+        viewHeaderKdMotor.setTextColor(Color.WHITE);
+        viewHeaderNama.setTextColor(Color.WHITE);
+        viewHeaderHarga.setTextColor(Color.WHITE);
+        viewHeaderAction.setTextColor(Color.WHITE);
+
+        // Mengatur padding header
+        viewHeaderKdMotor.setPadding(5, 1, 5, 1);
+        viewHeaderNama.setPadding(5, 1, 5, 1);
+        viewHeaderHarga.setPadding(5, 1, 5, 1);
+        viewHeaderAction.setPadding(5, 1, 5, 1);
+
+        // Menambahkan header ke baris tabel
+        barisTabel.addView(viewHeaderKdMotor);
+        barisTabel.addView(viewHeaderNama);
+        barisTabel.addView(viewHeaderHarga);
+        barisTabel.addView(viewHeaderAction);
+
+        tbMotor.addView(barisTabel, new TableLayout.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
+
+        try {
+            arrayMotor = new JSONArray(motor.tampilMotor());
+
+            // Mengiterasi dan menampilkan data
+            for (int i = 0; i < arrayMotor.length(); i++) {
+                JSONObject jsonChildNode = arrayMotor.getJSONObject(i);
+
+                // Ambil data dari nama tabel databse
+                String idmotor = jsonChildNode.optString("idmotor");
+                String kdmotor = jsonChildNode.optString("kdmotor");
+                String nama = jsonChildNode.optString("nama");
+                String harga = jsonChildNode.optString("harga");
+
+                System.out.println("kdmotor: " + kdmotor);
+                System.out.println("id: " + idmotor);
+                System.out.println("nama:" + nama);
+                System.out.println("harga:" + harga);
+
+                barisTabel = new TableRow(this);
+                // Memberi warna latar belakang bergantian
+                if (i % 2 == 0) {
+                    barisTabel.setBackgroundColor(Color.LTGRAY);
+                }
+
+                // Kolom KdMotor
+                TextView viewKdMotor = new TextView(this);
+                viewKdMotor.setText(kdmotor);
+                viewKdMotor.setPadding(5, 1, 5, 1);
+                barisTabel.addView(viewKdMotor);
+
+                // Kolom Nama
+                TextView viewNama = new TextView(this);
+                viewNama.setText(nama);
+                viewNama.setPadding(5, 1, 5, 1);
+                barisTabel.addView(viewNama);
+
+                // Kolom Harga
+                TextView viewHarga = new TextView(this);
+                viewHarga.setText(harga);
+                viewHarga.setPadding(5, 1, 5, 1);
+                barisTabel.addView(viewHarga);
+
+                // Membuat Button Edit pada Baris
+                buttonEdit.add(i, new Button(this));
+                buttonEdit.get(i).setId(Integer.parseInt(idmotor));
+                buttonEdit.get(i).setTag("Edit");
+                buttonEdit.get(i).setText("Edit");
+                buttonEdit.get(i).setOnClickListener(this);
+                barisTabel.addView(buttonEdit.get(i));
+
+                // Membuat Button Delete pada Baris
+                buttonDelete.add(i, new Button(this));
+                buttonDelete.get(i).setId(Integer.parseInt(idmotor));
+                buttonDelete.get(i).setTag("Delete");
+                buttonDelete.get(i).setText("Delete");
+                buttonDelete.get(i).setOnClickListener(this);
+                barisTabel.addView(buttonDelete.get(i));
+
+                tbMotor.addView(barisTabel, new TableLayout.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Hapus Motor bersadarkan ID
+    public void deleteMotor(int id) {
+        motor.deleteMotor(id);
+        // restart activity
+        finish();
+        startActivity(getIntent());
+    }
+
+    //Ambil data motor bersdasarkan Id
+    public void getMotorByKdmotor(int idmotor) {
+        String idmotorEdit = null;
+        String kdmotorEdit = null;
+        String namaEdit = null;
+        String hargaEdit = null;
+        JSONArray arrayPersonal;
+
+        try {
+            arrayPersonal = new JSONArray(motor.getMotorByKdmotor(idmotor));
+            for (int i = 0; i < arrayPersonal.length(); i++) {
+                JSONObject jsonChildNode = arrayPersonal.getJSONObject(i);
+                idmotorEdit = jsonChildNode.optString("idmotor");
+                kdmotorEdit = jsonChildNode.optString("kdmotor");
+                namaEdit = jsonChildNode.optString("nama");
+                hargaEdit = jsonChildNode.optString("harga");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        LinearLayout layoutInput = new LinearLayout(this);
+        layoutInput.setOrientation(LinearLayout.VERTICAL);
+
+        // buat id tersembunyi di alertbuilder
+        final TextView viewKdmotor = new TextView(this);
+        viewKdmotor.setText("Kode Motor " + String.valueOf(kdmotorEdit));
+        viewKdmotor.setTextColor(Color.WHITE);
+        viewKdmotor.setTextSize(20);
+        layoutInput.addView(viewKdmotor);
+
+        //membuat edit text di Allert builder
+        final EditText editIdMotor = new EditText(this);
+        editIdMotor.setText(idmotorEdit);
+        //layout Input.addView(editKdMotor);
+
+        //membuat edit text di Allert builder
+        final EditText editNama = new EditText(this);
+        editNama.setText(namaEdit);
+        layoutInput.addView(editNama);
+
+        //membuat edit text di Aliert builder
+        final EditText editHarga = new EditText(this);
+        editHarga.setText(hargaEdit);
+        editHarga.setInputType(android.text.InputType.TYPE_CLASS_NUMBER);
+        layoutInput.addView(editHarga);
+
+        AlertDialog.Builder builderEditMotor = new AlertDialog.Builder(this);
+        //builderEdi Biodata.setIcon(R.drawable.batagrams);
+        builderEditMotor.setTitle("Update Motor");
+        builderEditMotor.setView(layoutInput);
+
+        builderEditMotor.setPositiveButton("Update", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String idmotor = editIdMotor.getText().toString();
+                String kdmotor = viewKdmotor.getText().toString();
+                String nama = editNama.getText().toString();
+                String harga = editHarga.getText().toString();
+
+                // System.out.println("IdMotor:" + idmotor + "KdMotor: " + kdmotor + "Nama:" + nama + "Harga: " + harga);
+
+                String laporan = motor.updateMotor(editIdMotor.getText().toString(), viewKdmotor.getText().toString(), editNama.getText().toString(), editHarga.getText().toString());
+
+                Toast.makeText(DataMotorActivity.this, laporan, Toast.LENGTH_SHORT).show();
+                // tambahkan restart activity di sini.
+            }
         });
+
+        //membuat Button Cancel pada builder
+        builderEditMotor.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builderEditMotor.show();
+    }
+
+    //Metode tambah Motor
+    public void tambahMotor() {
+        //layout akan ditampilkan pada AlertDialog
+        LinearLayout layoutInput = new LinearLayout(this);
+        layoutInput.setOrientation(LinearLayout.VERTICAL);
+
+        final EditText editKdMotor = new EditText(this);
+        editKdMotor.setHint("KdMotor");
+        layoutInput.addView(editKdMotor);
+
+        final EditText editNama = new EditText(this);
+        editNama.setHint("Name");
+        layoutInput.addView(editNama);
+
+        final EditText editHarga = new EditText(this);
+        editHarga.setHint("harga");
+        editHarga.setInputType(android.text.InputType.TYPE_CLASS_NUMBER);
+        layoutInput.addView(editHarga);
+
+        AlertDialog.Builder builderInsertMotor = new AlertDialog.Builder(this);
+        //builderInsertMotor.setIcon(R.drawable.batagrams);
+        builderInsertMotor.setTitle("Insert Motor");
+        builderInsertMotor.setView(layoutInput);
+
+        builderInsertMotor.setPositiveButton("Insert", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String kdmotor = editKdMotor.getText().toString();
+                String nama = editNama.getText().toString();
+                String harga = editHarga.getText().toString();
+
+                // System.out.println("KdMotor: " + kdmotor + " Nama: " + nama + " Harga: " + harga);
+
+                String laporan = motor.insertMotor(kdmotor, nama, harga);
+                Toast.makeText(DataMotorActivity.this, laporan, Toast.LENGTH_SHORT).show();
+
+                // restart acrtivity
+                finish();
+                startActivity(getIntent());
+            }
+        });
+
+        builderInsertMotor.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builderInsertMotor.show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        // TODO Auto-generated method stub
+        for (int i = 0; i < buttonEdit.size(); i++) {
+            // jika yang diklik adalah button edit
+            if (view.getId() == buttonEdit.get(i).getId() && view.getTag().toString().trim().equals("Edit")) {
+                // Toast.makeText(MainActivity.this, "Edit:" + buttonEdit.get(i).getId(), Toast.LENGTH_SHORT).show();
+                int idmotor = buttonEdit.get(i).getId();
+                getMotorByKdmotor(idmotor);
+                // jika yang diklik adalah button delete
+            } else if (view.getId() == buttonDelete.get(i).getId() && view.getTag().toString().trim().equals("Delete")) {
+                // Toast.makeText(MainActivity.this, "Delete: " + buttonDelete.get(i).getId(), Toast.LENGTH_SHORT).show();
+                int idmotor = buttonDelete.get(i).getId();
+                deleteMotor(idmotor);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/kew_kredit_ilham1125100160_tii25/Motor.java
@@ -0,0 +1,125 @@
+package com.example.kew_kredit_ilham1125100160_tii25;
+
+public class Motor extends Koneksi {
+    private long id; // Variabel ID motor
+
+    // Inisialisasi Server dan URL API
+    Server server = new Server();
+    String SERVER = server.urlDatabase1();
+    String URL = "http://" + SERVER + "/pemograman-mobile-2-web/tbmotor.php"; // URL lengkap ke script PHP
+
+    String url = "";
+    String response = "";
+
+    /**
+     * Mengambil semua data motor dari server (operasi=view).
+     */
+    public String tampilMotor() {
+        try {
+            url = URL + "?operasi=view";
+            System.out.println("URL, Tampil Motor: " + url);
+            response = call(url);
+        } catch (Exception e) {
+            return response;
+        }
+        return response;
+    }
+
+    /**
+     * Mengambil data motor untuk keperluan dropdown/list dengan kolom id dan nama.
+     * (operasi=select_by_idnama).
+     */
+    public String tampilMotorbyIdNama() {
+        try {
+            url = URL + "?operasi=select_by_idnama";
+            System.out.println("URL Tampil Kreditor: " + url);
+            response = call(url);
+        } catch (Exception e) {
+            return response;
+        }
+        return response;
+    }
+
+    /**
+     * Menyimpan data motor baru ke database (operasi=insert).
+     */
+    public String insertMotor(String kdmotor, String nama, String harga) {
+        // Baris untuk replace karakter dihilangkan karena tidak lengkap/jelas di sumber
+        // nama nama.replace("20")
+        try {
+            url = URL + "?operasi=insert&kdmotor=" + kdmotor + "&nama=" + nama + "&harga=" + harga;
+            System.out.println("URL Insert Motor: " + url);
+            response = call(url);
+        } catch (Exception e) {
+            // H
+            return response;
+        }
+        return response;
+    }
+
+    /**
+     * Mengambil detail satu motor berdasarkan idmotor (operasi=get_motor_by_kdmotor).
+     */
+    public String getMotorByKdmotor(int idmotor) {
+        try {
+            url = URL + "?operasi=get_motor_by_kdmotor&idmotor=" + idmotor;
+            System.out.println("URL Get Motor: " + url);
+            response = call(url);
+        } catch (Exception e) {
+            return response;
+        }
+        return response;
+    }
+
+    /**
+     * Mengambil data motor berdasarkan kdmotor untuk keperluan kredit.
+     * (operasi=select_by_kdmotorkredit).
+     */
+    public String select_by_KdmotorKredit(String kdmotor) {
+        try {
+            url = URL + "?operasi=select_by_kdmotorkredit&kdmotor=" + kdmotor;
+            System.out.println("URL Get Motor: " + url);
+            response = call(url);
+        } catch (Exception e) {
+            return response;
+        }
+        return response;
+    }
+
+    /**
+     * Memperbarui data motor (nama dan harga) berdasarkan idmotor (operasi=update).
+     */
+    public String updateMotor(String idmotor, String kdmotor, String nama, String harga) {
+        // Baris untuk replace karakter dihilangkan karena tidak lengkap/jelas di sumber
+        // nama.replace("","120");
+        // kdmotor.replace("","20");
+        try {
+            url = URL + "?operasi=update&idmotor=" + idmotor + "&kdmotor=" + kdmotor + "&nama=" + nama + "&harga=" + harga;
+            System.out.println("URL Update Motor: " + url);
+            response = call(url);
+        } catch (Exception e) {
+            return response;
+        }
+        return response;
+    }
+
+    /**
+     * Menghapus data motor berdasarkan idmotor (operasi=delete).
+     */
+    public String deleteMotor(int idmotor) {
+        try {
+            url = URL + "?operasi=delete&idmotor=" + idmotor;
+            System.out.println("URL Hapus Motor: " + url);
+            response = call(url);
+        } catch (Exception e) {
+            return response;
+        }
+        return response;
+    }
+
+    public long getId() {
+        // TODO Auto-generated method stub
+        return id;
+        // return null;
+    }
+}
