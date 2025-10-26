8caa825dcaff3fd60b5739856869e3df05bfd131
Terabash <git@terabash.com>
2025-10-26 16:41:49 +0700

    Pemrograman Mobile-2 – DESAIN MENU – Sesi02 - 20Sept25 ABS-Kuliah Universitas STEKOM

--- app/src/main/java/com/example/kew_kredit_ilham1125100160_tii25/HomeActivity.java
+++ app/src/main/java/com/example/kew_kredit_ilham1125100160_tii25/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.example.kew_kredit_ilham1125100160_tii25;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -15,10 +18,46 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btDataPetugasHome = (Button) findViewById(R.id.btDataPetugasHome);
+        Button btDataMotorHome = (Button) findViewById(R.id.btDataMotorHome);
+        Button btDataKreditorHome = (Button) findViewById(R.id.btDataKreditorHome);
+
+        btDataPetugasHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                KlikbtDataPetugasHome();
+            }
         });
+
+        btDataMotorHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                KlikbtDataMotorHome();
+            }
+        });
+
+        btDataKreditorHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                KlikbtDataKreditorHome();
+            }
+        });
+    }
+
+    public void KlikbtDataPetugasHome() {
+        Intent i = new Intent(getApplicationContext(), DataPetugasActivity.class);
+        ;//target = nama class
+        startActivity(i);
+    }
+
+    public void KlikbtDataMotorHome() {
+        Intent i = new Intent(getApplicationContext(), DataMotorActivity.class);
+        startActivity(i);
+    }
+
+    public void KlikbtDataKreditorHome() {
+        Intent i = new Intent(getApplicationContext(), DataKreditorActivity.class);
+        startActivity(i);
     }
 }
